93cd6670c2a15c92ca5e21f400e09adce136ae02
AmBakk <aminebakk@hotmail.fr>
2024-09-10 00:19:34 +0200

    updates

--- util/update_gsheets.py
+++ util/update_gsheets.py
@@ -1,34 +1,31 @@
-from datetime import datetime
+import json
+import os
 
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
-import numpy as np
 
-# Define the path to the service account credentials JSON file (created dynamically in the workflow)
-SERVICE_ACCOUNT_FILE = '../player-data-project-467fdd605960.json'
+service_account_info = os.getenv('GSHEETS_SERVICE_ACCOUNT')
 
-# Define Google Sheets details
-SPREADSHEET_ID = '1fbFxl0tn0zAD8wpCfITfR5w4uBRr9wsu5yIS03FB2z4'  # Replace with your actual spreadsheet ID
-SHEET_NAME = 'player-data'  # Sheet/tab name where you want to upload the data
+if service_account_info:
+    service_account_info = json.loads(service_account_info)
+else:
+    raise Exception("Service account credentials not found in environment variables.")
 
-# Define the scope for Google Sheets API
+SPREADSHEET_ID = '1fbFxl0tn0zAD8wpCfITfR5w4uBRr9wsu5yIS03FB2z4'
+SHEET_NAME = 'player-data'
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-# Authenticate with Google Sheets API
-creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+creds = Credentials.from_service_account_file(service_account_info, scopes=SCOPES)
 client = gspread.authorize(creds)
 
-# Convert the final DataFrame to Pandas
 final_df_pd = pd.read_csv('../data/final/final_df.csv')
 
-# Open the Google Sheet and select the worksheet
 sheet = client.open_by_key(SPREADSHEET_ID).worksheet(SHEET_NAME)
 
-# Clear the existing data in the sheet
 sheet.clear()
 
-# Update the sheet with the new data (including headers)
 sheet.update([final_df_pd.columns.values.tolist()] + final_df_pd.values.tolist())
 
 print("Data successfully uploaded to Google Sheets.")
